ea60f41f67b3f4fe6dc5c0afdac91246d4b1bfad
Jason Pham <me@jasonpham.ca>
2017-01-21 10:59:47 -0500

    Create news module

--- python/news.py
+++ python/news.py
@@ -1,13 +1,37 @@
-#
-#
-#   news.py
-#
-#   Using the News API, it aggregates news based off
-#   a search query
-#
-#
-
-#   API Key
-#   712e402419e84ccd87fd1e5958456192
+import json
+from urllib.request import urlopen
 
 
+class News:
+    def __init__(self, query):
+        self._query = query
+
+    def getArticles(self):
+        filteredArticles = []
+
+        url = 'http://newsapi.org/v1/articles?'
+        with open('sources.json') as data_file:
+            sources = json.load(data_file)
+
+        for source in sources['sources']:
+            root = 'source=' + source['id'] + '&apiKey='
+            res = urlopen(url + root).read().decode('utf-8')
+            articles = json.loads(res)
+
+            filteredArticles.append(self.searchArticles(articles['articles']))
+
+        print(len(filteredArticles))
+
+    def searchArticles(self, articles):
+        filteredArticles = []
+
+        for article in articles:
+            try:
+                if self._query.lower() in article['description'].lower() or self._query.lower() \
+                        in article['title'].lower():
+                    filteredArticles.append(article)
+            except AttributeError:
+                # DO nothing lol
+                print("Error happened")
+
+        return filteredArticles
