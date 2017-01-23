6968a1e02dcccdd6eafa7e47d9784cf3ff929472
Eric Liu <ericliu314@gmail.com>
2017-01-22 22:41:29 -0500

    Local caching

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sentiment
 
 app = Flask(__name__)
 
+cache = dict()
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html', text='Try searching for something.')
@@ -12,61 +14,82 @@ def index():
 @app.route('/search', methods=['GET', 'POST'])
 def send():
     if request.method == 'POST':
+
         query = request.form['search']
 
-        news = News(query)
-        articles = news.getArticles()
-        descriptions = news.getDescriptions()
+        if query not in cache:
 
-        sentiments = list()
-        result = ''
-        max = 0
-        min = 1
+            news = News(query)
+            articles = news.getArticles()
+            descriptions = news.getDescriptions()
 
-        if len(articles) <= 0:
-            return render_template('index.html', text='No results.')
+            sentiments = list()
+            result = ''
+            max = 0
+            min = 1
 
-        for article in articles:
-            description = article['description']
+            if len(articles) <= 0:
+                return render_template('index.html', text='No results.')
 
-            if len(description) <= 0:
-                continue
+            for article in articles:
+                description = article['description']
 
-            sentimentValue = sentiment.getSentiment(str(article['description']))
-            sentiments.append(sentimentValue)
+                if len(description) <= 0:
+                    continue
 
-            if sentimentValue > max:
-                max = sentimentValue
+                sentimentValue = sentiment.getSentiment(str(article['description']))
+                sentiments.append(sentimentValue)
 
-            if sentimentValue < min:
-                min = sentimentValue
+                if sentimentValue > max:
+                    max = sentimentValue
 
-            article['sentimentValue'] = article.get('sentimentValue', '') + sentiment.getSentimentText(sentimentValue)
+                if sentimentValue < min:
+                    min = sentimentValue
 
-            result += '<div class="article-container">' \
-                      '<div class="article-element">' \
-                      '<img class="image" src="' + article['urlToImage'] + '' \
-                      '" alt="' + article['title'] + '"></div>' \
-                      '<div class="article-element"><div class="article-title"><a href="' + article['url'] + ''\
-                      '">' + article['title'] + '</a></div><div class="article-description">' + article['description'] + ''\
-                      '</div><div class="article-link"><a href="' + article['url'] + '">Read More &rarr;</a></div>' \
-                      '</div><div class="article-element"><div class="article-sentiment-title">Consensus</div>' \
-                      '<div class="article-sentiment" style="color:' + sentiment.getSentimentColor(sentimentValue) + ''\
-                      ';">' + sentiment.getSentimentText(sentimentValue) + '</div></div></div>'
+                article['sentimentValue'] = article.get('sentimentValue', '') + sentiment.getSentimentText(sentimentValue)
 
-        spread = max - min
-        sumSentiments = sum(sentiments)
-        averageSentiment = sumSentiments / len(sentiments)
-        sentimentValue = sentiment.getSentimentText(averageSentiment)
-        color = sentiment.getSentimentColor(averageSentiment)
+                result += '<div class="article-container">' \
+                          '<div class="article-element">' \
+                          '<img class="image" src="' + str(article['urlToImage']) + '' \
+                          '" alt="' + str(article['title']) + '"></div>' \
+                          '<div class="article-element"><div class="article-title"><a href="' + str(article['url']) + ''\
+                          '">' + str(article['title']) + '</a></div><div class="article-description">' + str(article['description']) + ''\
+                          '</div><div class="article-link"><a href="' + str(article['url']) + '">Read More &rarr;</a></div>' \
+                          '</div><div class="article-element"><div class="article-sentiment-title">Consensus</div>' \
+                          '<div class="article-sentiment" style="color:' + str(sentiment.getSentimentColor(sentimentValue)) + ''\
+                          ';">' + str(sentiment.getSentimentText(sentimentValue)) + '</div></div></div>'
 
-        return render_template('search.html',
-                               queryHeader=query,
-                               sentimentScore='{0:.6f}'.format(averageSentiment),
-                               sentimentValue=sentimentValue,
-                               spread='{0:.6f}'.format(spread),
-                               color=color,
-                               result=result)
+            spread = max - min
+            sumSentiments = sum(sentiments)
+            averageSentiment = sumSentiments / len(sentiments)
+            sentimentValue = sentiment.getSentimentText(averageSentiment)
+            color = sentiment.getSentimentColor(averageSentiment)
+
+            newCache = {'queryHeader': query,
+                        'sentimentScore': averageSentiment,
+                        'sentimentValue': sentimentValue,
+                        'spread': spread,
+                        'color': color,
+                        'result': result}
+
+            cache[query] = newCache
+
+            return render_template('search.html',
+                                   queryHeader=query,
+                                   sentimentScore='{0:.6f}'.format(averageSentiment),
+                                   sentimentValue=sentimentValue,
+                                   spread='{0:.6f}'.format(spread),
+                                   color=color,
+                                   result=result)
+        else:
+            return render_template('search.html',
+                                   queryHeader=cache[query]['queryHeader'],
+                                   sentimentScore='{0:.6f}'.format(cache[query]['sentimentScore']),
+                                   sentimentValue=cache[query]['sentimentValue'],
+                                   spread='{0:.6f}'.format(cache[query]['spread']),
+                                   color=cache[query]['color'],
+                                   result=cache[query]['result'])
+
     else:
         return render_template('index.html', text='Try searching for something.')
 
